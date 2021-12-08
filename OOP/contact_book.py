5d470b8539ea5dd797d45ae7aabb425450f06e61
LukaszMinkisiewiczAllegro <lukasz.minkisiewicz@allegro.pl>
2021-12-08 15:33:51 +0100

    Pirwsze zadanie z modulu OOP

--- OOP/contact_book.py
+++ OOP/contact_book.py
@@ -20,9 +20,49 @@ class ContactCard:
     def contact(self):
         return f'Kontaktuje sie z {self.name} {self.surname} {self.position} {self.email}'
 
+
+class BaseContact:
+    def __init__(self, name, surname, phone, email):
+        self.name = name
+        self.surname = surname
+        self.phone = phone
+        self.email = email
+    
+    def contact(self):
+        return f'Wybieram numer {self.phone} i dzwonie do {self.name} {self.surname}'
+
+    @property
+    def name_length(self):
+        return len(self.name) + len(self.surname) + 1
+
+class BusinessContact(BaseContact):
+    def __init__(self, buss_phone, position, company,*args, **kwargs):
+        self.buss_phone = buss_phone
+        self.position = position
+        self.company = company
+        super().__init__(*args, **kwargs)
+
+    def contact(self):
+        return f'Wybieram numer {self.buss_phone} i dzwonie do {self.name} {self.surname}'    
+
+
 def create_contact():
     new_contact = ContactCard(fake.unique.first_name(),fake.last_name(), fake.company(), fake.job(), fake.email())
     return new_contact
+
+
+def create_contacts(a = 'Base', how_many = 1):
+    cnt = 0
+    contact_list_1 = []
+    while cnt < how_many:
+        if type == 'Base':
+            contact_list_1.append(BaseContact(fake.unique.first_name(),fake.last_name(),fake.phone_number(), fake.email()))
+        else:
+            contact_list_1.append(BusinessContact(fake.phone_number(),fake.job(), fake.company(), fake.unique.first_name(),fake.last_name(),fake.phone_number(), fake.email(),))
+        
+        cnt += 1
+    
+    return contact_list_1
 
 
 contact_1 = create_contact()
@@ -44,3 +84,9 @@ for contact in by_name:
 
 print(contact_1.name_length)
 
+
+test_contacts = create_contacts(how_many=5)
+print(test_contacts)
+
+for obj in test_contacts:
+    print(obj.contact())
