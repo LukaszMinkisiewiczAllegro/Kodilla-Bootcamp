fb0c1a5429f17b6f79abc9c035a3332bdce2a9c0
LukaszMinkisiewiczAllegro <lukasz.minkisiewicz@allegro.pl>
2021-12-09 11:45:36 +0100

    Poprawienie movie_rental

--- OOP/movie_rental.py
+++ OOP/movie_rental.py
@@ -1,5 +1,6 @@
 import random
 from faker import Faker
+from datetime import date
 rental = []
 
 class Movie:
@@ -22,7 +23,7 @@ class TvSeries(Movie):
         self.season = season
 
     def __str__(self):
-        return f'{self.title} S{self.season}E{self.episode}'
+        return f'{self.title} S{self.season:02d}E{self.episode:02d}'
 
 def add_movie(title, year, gendre, played, movies_list = rental):
     rental.append(Movie(title,year,gendre,played))
@@ -68,6 +69,10 @@ def top_titles(how_many):
     movies = sorted(rental, key = lambda k: k.played, reverse = True)
     return movies[:how_many]
 
+def add_full_season(title, year, gendre, season, episodes):
+    for episode in range(1, episodes + 1):
+        rental.append(TvSeries(title, year, gendre, 0, episode, season))
+
 
 faker = Faker()
 print("Biblioteka filmów")
@@ -85,13 +90,15 @@ rental.append(TvSeries('Friends', 1994, 'Comedy', 0, 4,4))
 rental.append(TvSeries('Sherlock', 2010, 'Drama', 0, 5,5))
 rental.append(TvSeries('Stranger Things', 2016, 'Drama', 0, 6,6))
 
+add_full_season('McDonald', 2004, 'Drama', 1, 20)
+
 for x in range(1000):
     generate_views()
 
 for movie in rental:
-    print(f'{movie.title} {movie.played}')
+    print(f'{movie} {movie.played}')
     
-print("Najpopularniejsze filmy i seriale dnia")
+print(f"Najpopularniejsze filmy i seriale dnia {date.today()}")
 
 top_3 = top_titles(3)
 for i in top_3:
